1236f2fa8e3389e74010ee8018ef7298cb43ff68
freshfish1080 <45006160+freshfish1080@users.noreply.github.com>
2022-05-03 06:05:49 +0900

    2022-05-03

---
+++ src/ChildArea.jsx
@@ -0,0 +1,21 @@
+import { render } from "react-dom";
+import { memo } from "react";
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = memo((props) => {
+  const { open } = props;
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+        </div>
+      ) : null}
+    </>
+  );
+});
